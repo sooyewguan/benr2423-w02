27ccdd3e53f76366471b2caa429048143f88fc2e
Soo Yew Guan <sooyewguan@Soos-MacBook-Pro.local>
2022-03-22 10:11:30 +0800

    week 3 solution

--- week02.js
+++ week02.js
@@ -2,60 +2,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://m001-student:p4ssw0rd@sandbox.epk5x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect(async err => {
+client.connect( err => {
 	if (err) {
 		console.log(err.message)
 		return
 	}
 	console.log('Connected to MongoDB');
-
-	client.db('myFirstDatabase').collection('customers').deleteMany({
-		name: 'Ali', 
-		age: 30 
-	}).then(result => {
-		console.log(result.deletedCount);
-	})
-	// client.db('myFirstDatabase').collection('customers').updateOne({
-	// 	name: 'John'
-	// }, {
-	// 	$set: {
-	// 		name: "John Doe",
-	// 		age: 35
-	// 	}
-	// }, { upsert: true }).then(res => {
-	// 	console.log(res)
-	// })
-
-	// client.db('myFirstDatabase').collection('customers').insertOne({
-	// 	name: 'John',
-	// 	age: 30,
-	// 	location: 'New York',
-	// 	isActive: true,
-	// 	tags: ['tag1', 'tag2'],
-	// }).then(result => {
-	// 	console.log(result);
-	// });
-
-	// console.time('insert');
-	// let result = await client.db('myFirstDatabase').collection('customers').insertMany([
-	// 	{
-	// 		name: 'Ali',
-	// 		age: 30,
-	// 		location: 'New York',
-	// 		isActive: true,
-	// 		tags: ['tag1', 'tag2'],
-	// 	}, 
-	// 	{
-	// 		name: 'Jon',
-	// 		age: 40,
-	// 		location: 'Melaka',
-	// 		isActive: true,
-	// 		tags: ['tag1', 'tag2'],
-	// 	}
-	// ])
-	// console.timeEnd('insert')
-
-	// console.log('Inserted 1 document', result);
 
 	// client.db().admin().listDatabases().then(result => {
 	// 	console.log(result['databases'][7]['name']);	
--- week03.js
+++ week03.js
@@ -1,1 +1,64 @@
-console.log('test');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://m001-student:p4ssw0rd@sandbox.epk5x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(async err => {
+	if (err) {
+		console.log(err.message)
+		return
+	}
+	console.log('Connected to MongoDB');
+	console.time('insert');
+
+	let result = await client.db('Week03').collection('companies').deleteMany({
+		name: 'Zenfolio'
+	})
+	// let result = await client.db('Week03').collection('companies').updateOne(
+	// 	{ name: 'utem' },
+	// 	{ $set: { address: 'Zenfolio Inc.' } },
+	// 	{ upsert: true }
+	// )
+	// let result = await client.db('Week03').collection('companies').insertMany([
+	// 	{
+	// 		name: 'Zenfolio',
+	// 		address: {
+	// 			street: '123 Main St',
+	// 			city: 'New York',
+	// 			state: 'NY',
+	// 			zip: '10001'
+	// 		}
+	// 	},
+	// 	{
+	// 		name: 'Google',
+	// 		address: {
+	// 			street: '1600 Amphitheatre Pkwy',
+	// 			city: 'Mountain View',
+	// 			state: 'CA',
+	// 			zip: '94043'
+	// 		}
+	// 	}
+	// ])
+	// client.db('Week03').collection('companies').insertOne({
+	// 	name: 'Zenfolio',
+	// 	address: {
+	// 		street: '123 Main St',
+	// 		city: 'New York',
+	// 		state: 'NY',
+	// 		zip: '10001'
+	// 	},
+	// 	employees: [
+	// 		{
+	// 			name: 'John',
+	// 			age: 25,
+	// 			position: 'CEO'
+	// 		},
+	// 	]
+	// }).then(result => {
+	// 	console.timeEnd('insert');
+	// 	console.log(result);
+	// })
+
+	console.timeEnd('insert');
+	console.log(result);
+	console.log('completed')
+});
